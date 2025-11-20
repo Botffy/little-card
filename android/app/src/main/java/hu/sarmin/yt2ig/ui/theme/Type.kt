a29847ea03ec562449351ff143f1fa125fd7b688
Ármin Scipiades <scipiades.armin@gmail.com>
2025-11-20 19:52:18 +0100

    Fix Inter font not really used

--- android/app/src/main/java/hu/sarmin/yt2ig/ui/theme/Type.kt
+++ android/app/src/main/java/hu/sarmin/yt2ig/ui/theme/Type.kt
@@ -1,7 +1,6 @@
 package hu.sarmin.yt2ig.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -13,10 +12,22 @@ private val Inter = FontFamily(
     Font(R.font.inter_semibold, FontWeight.SemiBold),
 )
 
-val Typography = Typography(
-    bodyLarge = TextStyle(fontFamily = Inter),
-    bodyMedium = TextStyle(fontFamily = Inter),
-    bodySmall = TextStyle(fontFamily = Inter),
-    titleMedium = TextStyle(fontFamily = Inter, fontWeight = FontWeight.SemiBold),
-    labelMedium = TextStyle(fontFamily = Inter, fontWeight = FontWeight.Medium)
-)
+val interTypography = Typography().run {
+    copy(
+        displayLarge = displayLarge.copy(fontFamily = Inter),
+        displayMedium = displayMedium.copy(fontFamily = Inter),
+        displaySmall = displaySmall.copy(fontFamily = Inter),
+        headlineLarge = headlineLarge.copy(fontFamily = Inter),
+        headlineMedium = headlineMedium.copy(fontFamily = Inter),
+        headlineSmall = headlineSmall.copy(fontFamily = Inter),
+        titleLarge = titleLarge.copy(fontFamily = Inter),
+        titleMedium = titleMedium.copy(fontFamily = Inter),
+        titleSmall = titleSmall.copy(fontFamily = Inter),
+        bodyLarge = bodyLarge.copy(fontFamily = Inter),
+        bodyMedium = bodyMedium.copy(fontFamily = Inter),
+        bodySmall = bodySmall.copy(fontFamily = Inter),
+        labelLarge = labelLarge.copy(fontFamily = Inter),
+        labelMedium = labelMedium.copy(fontFamily = Inter),
+        labelSmall = labelSmall.copy(fontFamily = Inter),
+    )
+}
